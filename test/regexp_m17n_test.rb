6078e978d1bcd2e19f36f8be76440aa4d12de0d4
Joseph Benden <joe@benden.us>
2014-08-07 11:17:58 -0700

    Use translation and Iconv to solve encoding issue.

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -1,11 +1,13 @@
-# encoding: utf-8
 require 'minitest/autorun'
 require_relative '../lib/regexp_m17n'
 
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
+      begin
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      rescue Encoding::ConverterNotFoundError
+      end
     end
   end
 end
